4b1c590204baf182667a991e542010953aab84d7
kodaki <kodakicodes@gmail.com>
2025-12-26 21:20:23 +0530

    feat: add LLM integratiopn to chat panel

--- client/src/components/ChatPanel.tsx
+++ client/src/components/ChatPanel.tsx
@@ -1,10 +1,46 @@
 import { useState } from 'react'
+import axios, { AxiosError } from 'axios'
+import type { ExpectedTemplateResponse, TemplateErrorResponse } from '../types'
 
 export function ChatPanel() {
     const [input, setInput] = useState('')
+    
+    async function hitChatEP(message: string) {
+        try {
+            const llmResponse: string  = await axios.post('http://localhost:3000/chat', { message });
+            console.log("LLM Response:", llmResponse);
+        } catch (error) {
+            console.error("Error hitting chat endpoint:", error);
+        }
+    }
+
+    function handleStepResponse(uiPrompt: string) {
+        console.log("UI Prompt for Steps Panel:", uiPrompt);
+    }
+
+    async function hitLLM(message: string) {
+        try {
+            const response = await axios.post<ExpectedTemplateResponse>('http://localhost:3000/template', { prompt: message });
+            console.log("Sending message to LLM:", message);
+            const llmPrompt = JSON.stringify(response.data.prompts) +'\n\n'+message;
+            const uiPrompt = JSON.stringify(response.data.uiPrompts);
+            hitChatEP(llmPrompt);
+            handleStepResponse(uiPrompt);
+            
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const axiosError = error as AxiosError<TemplateErrorResponse>;
+                console.error("LLM Error:", axiosError.response?.data?.msg);
+            } else {
+                console.error("Unexpected error:", error);
+            }
+        }
+    }
 
     const handleSend = () => {
-        // TODO: implement send logic
+        if (!input.trim()) return;
+        hitLLM(input);
+        setInput('');
     }
 
     return (
